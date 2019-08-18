bc09215bd517ffe36fc3ca43a7e376fda38199d9
Cmack704 <craigmack704@gmail.com>
2019-08-18 18:57:09 -0400

    edited schema and / route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,21 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all customers
+  app.get("/api/customers", function(req, res) {
+    db.Customers.findAll({}).then(function(results) {
+      res.json(results);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    // eslint-disable-next-line prettier/prettier
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new customers
+  app.post("/api/customers", function(req, res) {
+    db.Customers.create({
+      name: req.body.name,
+      email: req.body.email,
+      number: req.body.number
+    }).then(function(results) {
+      res.json(results);
     });
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,7 @@
-var db = require("../models");
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
   app.get("/dog", function(req, res) {
@@ -17,16 +10,6 @@ module.exports = function(app) {
 
   app.get("/cat", function(req, res) {
     res.render("cat");
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    // eslint-disable-next-line prettier/prettier
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
   });
 
   // Render 404 page for any unmatched routes

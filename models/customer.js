0a711dc71e32b98092133542fcb385a06bb78ffd
Craig McAlister <craigmack704@gmail.com>
2019-08-21 14:09:34 -0400

    rendered api results to handlebars page

--- models/customer.js
+++ models/customer.js
@@ -2,18 +2,11 @@ module.exports = function(sequelize, DataTypes) {
   var Customers = sequelize.define("customers", {
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
+      allowNull: false
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1],
-        isEmail: true
-      }
+      allowNull: false
     },
     number: DataTypes.STRING
   });
